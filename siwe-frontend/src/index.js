f19484fd1c24d8404cb2e3dfed3a40d5023a5d45
Hermann EHO <hermannleboss@gmail.com>
2023-05-14 14:38:08 +0000

    feat: connect frontend to backend

--- siwe-frontend/src/index.js
+++ siwe-frontend/src/index.js
@@ -5,34 +5,56 @@ const domain = window.location.host;
 const origin = window.location.origin;
 const provider = new BrowserProvider(window.ethereum);
 
-function createSiweMessage (address, statement) {
+const BACKEND_ADDR = "http://localhost:3000";
+
+async function createSiweMessage(address, statement) {
+    const res = await fetch(`${BACKEND_ADDR}/nonce`);
     const message = new SiweMessage({
         domain,
         address,
         statement,
         uri: origin,
         version: '1',
-        chainId: '1'
+        chainId: '1',
+        nonce: await res.text()
     });
     return message.prepareMessage();
 }
 
-function connectWallet () {
+function connectWallet() {
     provider.send('eth_requestAccounts', [])
         .catch(() => console.log('user rejected request'));
 }
 
-async function signInWithEthereum () {
+let message = null;
+let signature = null;
+
+async function signInWithEthereum() {
     const signer = await provider.getSigner();
-    const message = createSiweMessage(
+
+    message = await createSiweMessage(
         signer.address,
         'Sign in with Ethereum to the app.'
     );
-    console.log(await signer.signMessage(message));
+    console.log(message);
+    signature = await signer.signMessage(message);
+    console.log(signature);
 }
 
-// Buttons from the HTML page
+async function sendForVerification() {
+    const res = await fetch(`${BACKEND_ADDR}/verify`, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ message, signature }),
+    });
+    console.log(await res.text());
+}
+
 const connectWalletBtn = document.getElementById('connectWalletBtn');
 const siweBtn = document.getElementById('siweBtn');
+const verifyBtn = document.getElementById('verifyBtn');
 connectWalletBtn.onclick = connectWallet;
 siweBtn.onclick = signInWithEthereum;
+verifyBtn.onclick = sendForVerification;
