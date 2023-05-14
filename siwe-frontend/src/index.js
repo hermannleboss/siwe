e040a31df88363177a16ff8d61925d09be83071b
Hermann EHO <hermannleboss@gmail.com>
2023-05-14 16:53:42 +0000

    feat: manage ESN

--- siwe-frontend/src/index.js
+++ siwe-frontend/src/index.js
@@ -5,6 +5,16 @@ const domain = window.location.host;
 const origin = window.location.origin;
 const provider = new BrowserProvider(window.ethereum);
 
+const profileElm = document.getElementById('profile');
+const noProfileElm = document.getElementById('noProfile');
+const welcomeElm = document.getElementById('welcome');
+
+const ensLoaderElm = document.getElementById('ensLoader');
+const ensContainerElm = document.getElementById('ensContainer');
+const ensTableElm = document.getElementById('ensTable');
+
+let address;
+
 const BACKEND_ADDR = "http://localhost:3000";
 async function createSiweMessage(address, statement) {
     const res = await fetch(`${BACKEND_ADDR}/nonce`, {
@@ -29,9 +39,13 @@ function connectWallet() {
 
 async function signInWithEthereum() {
     const signer = await provider.getSigner();
+    profileElm.classList = 'hidden';
+    noProfileElm.classList = 'hidden';
+    welcomeElm.classList = 'hidden';
 
+    address = await signer.getAddress()
     const message = await createSiweMessage(
-        await signer.getAddress(),
+        address,
         'Sign in with Ethereum to the app.'
     );
     const signature = await signer.signMessage(message);
@@ -44,14 +58,60 @@ async function signInWithEthereum() {
         body: JSON.stringify({ message, signature }),
         credentials: 'include'
     });
+
+    if (!res.ok) {
+        console.error(`Failed in getInformation: ${res.statusText}`);
+        return
+    }
     console.log(await res.text());
+
+    displayENSProfile();
 }
 
 async function getInformation() {
     const res = await fetch(`${BACKEND_ADDR}/personal_information`, {
         credentials: 'include',
     });
-    console.log(await res.text());
+
+    if (!res.ok) {
+        console.error(`Failed in getInformation: ${res.statusText}`);
+        return
+    }
+
+    let result = await res.text();
+    console.log(result);
+    address = result.split(" ")[result.split(" ").length - 1];
+    displayENSProfile();
+}
+
+async function displayENSProfile() {
+    const ensName = await provider.lookupAddress(address);
+
+    if (ensName) {
+        profileElm.classList = '';
+
+        welcomeElm.innerHTML = `Hello, ${ensName}`;
+        let avatar = await provider.getAvatar(ensName);
+        if (avatar) {
+            welcomeElm.innerHTML += ` <img class="avatar" src=${avatar}/>`;
+        }
+
+        ensLoaderElm.innerHTML = 'Loading...';
+        ensTableElm.innerHTML.concat(`<tr><th>ENS Text Key</th><th>Value</th></tr>`);
+        const resolver = await provider.getResolver(ensName);
+
+        const keys = ["email", "url", "description", "com.twitter"];
+        ensTableElm.innerHTML += `<tr><td>name:</td><td>${ensName}</td></tr>`;
+        for (const key of keys)
+            ensTableElm.innerHTML += `<tr><td>${key}:</td><td>${await resolver.getText(key)}</td></tr>`;
+        ensLoaderElm.innerHTML = '';
+        ensContainerElm.classList = '';
+    } else {
+        welcomeElm.innerHTML = `Hello, ${address}`;
+        noProfileElm.classList = '';
+    }
+
+    welcomeElm.classList = '';
 }
 
 const connectWalletBtn = document.getElementById('connectWalletBtn');
